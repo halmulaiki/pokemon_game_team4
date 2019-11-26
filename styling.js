72750afb5c507a53fe95f1711e0ea0934bdea4fb
Claire Ramos <Bella@Claires-MacBook-Pro.local>
2019-11-26 16:21:33 -0500

    added code for different trainer pokemon, added pictures for pokemon on trainers teams

--- styling.js
+++ styling.js
@@ -5,6 +5,9 @@ let pokeSprites = document.getElementById('pokeSprites')
 let statbox1 = document.getElementById('statbox1')
 let statbox2 = document.getElementById('statbox2')
 let statbox3 = document.getElementById('statbox3')
+let pokesprite1 = document.getElementById('pokesprite1')
+let pokesprite2 =document.getElementById('pokesprite2')
+let pokesprite3 = document.getElementById('pokesprite3')
 
 // Turning the information windows off upon loading the website
 
@@ -14,16 +17,90 @@ statbox1.style.display = 'none'
 statbox2.style.display = 'none'
 statbox3.style.display = 'none'
 
-// Function to reveal trainer information
+// Function to reveal trainer 1 information
 
 function trainer1() {
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    
+    pokesprite1.setAttribute("src", 'img/mawile.png')
+    pokesprite2.setAttribute("src", 'img/sylveon.png')
+    pokesprite3.setAttribute("src", 'img/tapulele.png')
 }
 
 function pokestat1() {
     statbox1.style.display = 'grid'
-    
+}
 
+function pokestat2() {
+    statbox2.style.display = 'grid'
+}
+
+function pokestat3() {
+    statbox3.style.display = 'grid'
+}
+
+// Function to reveal Trainer 2 information
+
+function trainer2() {
+    charPortrait.style.display = 'grid'
+    pokeSprites.style.display = 'grid'
+    pokesprite1.setAttribute("src", 'img/incineroar.png')
+    pokesprite2.setAttribute("src", 'img/blaziken.png')
+    pokesprite3.setAttribute("src", 'img/charizard.png')
+}
+
+function pokestat1() {
+    statbox1.style.display = 'grid'
+}
+
+function pokestat2() {
+    statbox2.style.display = 'grid'
+}
+
+function pokestat3() {
+    statbox3.style.display = 'grid'
+}
+
+// Function to reveal Trainer 3 information
+
+function trainer3() {
+    charPortrait.style.display = 'grid'
+    pokeSprites.style.display = 'grid'
+    pokesprite1.setAttribute("src", 'img/flygon.png')
+    pokesprite2.setAttribute("src", 'img/dragonite.png')
+    pokesprite3.setAttribute("src", 'img/hydreigon.png')  
+}
+
+function pokestat1() {
+    statbox1.style.display = 'grid'
+}
+
+function pokestat2() {
+    statbox2.style.display = 'grid'
+}
+
+function pokestat3() {
+    statbox3.style.display = 'grid'
+}
+
+// Function to reveal Trainer 4 information
+
+function trainer4() {
+    charPortrait.style.display = 'grid'
+    pokeSprites.style.display = 'grid'
+    pokesprite1.setAttribute("src", 'img/gengar.png')
+    pokesprite2.setAttribute("src", 'img/golurk.png')
+    pokesprite3.setAttribute("src", 'img/doublade.png')   
+}
+
+function pokestat1() {
+    statbox1.style.display = 'grid'
+}
+
+function pokestat2() {
+    statbox2.style.display = 'grid'
+}
+
+function pokestat3() {
+    statbox3.style.display = 'grid'
 }
