98f75a0415a39bfbdf1347481a55c73c6554ffbc
juddin1995 <39656141+juddin1995@users.noreply.github.com>
2019-11-26 13:11:11 -0500

    pokemon class added

--- library.js
+++ library.js
@@ -37,3 +37,46 @@ class Trainer {
     }
   }
 }
+
+class Pokemon {
+  constructor(data) {
+    this.name = data.name;
+    this.sprite = data.sprites.front_default;
+    this.hp = data.stats[4].base_stat;
+    this.attack = data.stats[5].base_stat;
+    this.defense = data.stats[3].base_stat;
+    this.ability = data.abilities["0"].ability.name;
+  }
+  display() {
+    let grid = document.getElementById("pokedex")
+
+    let div = document.createElement('div')
+    grid.appendChild(div)
+    div.className = "card"
+
+    let pokeImage = document.createElement('img');
+    pokeImage.src = `${this.sprite}`;
+    div.appendChild(pokeImage)
+
+    let name = document.createElement('h2')
+    name.innerHTML = "Name: " + this.name
+    div.appendChild(name)
+
+    let hp = document.createElement('h3')
+    hp.innerHTML = "HP: " + this.hp
+    div.appendChild(hp)
+
+    let attack = document.createElement('h3')
+    attack.innerHTML = "Attack: " + this.attack
+    div.appendChild(attack)
+
+    let defense = document.createElement('h3')
+    defense.innerHTML = "Defense: " + this.defense
+    div.appendChild(defense);
+
+    let ability = document.createElement('h3')
+    ability.innerHTML = "Ability: " + this.ability
+    div.appendChild(ability)
+    console.log(this.ability);
+  }
+}
