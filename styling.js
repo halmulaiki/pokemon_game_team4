3bf82900f9fba743244e9b622b6631d7ab135310
Claire <claire.isabella.ramos@gmail.com>
2019-11-27 01:10:39 -0500

    final styling done on website, added nameplate for trainers clicked on. I'm tired.

--- styling.js
+++ styling.js
@@ -2,6 +2,7 @@
 
 let charPortrait = document.getElementById('charPortrait')
 let gymLeader = document.getElementById('gymLeader')
+let trainerName = document.getElementById('trainerName')
 let pokeSprites = document.getElementById('pokeSprites')
 let statbox1 = document.getElementById('statbox1')
 let statbox2 = document.getElementById('statbox2')
@@ -27,6 +28,7 @@ function trainer1() {
     pokeSprites.style.display = 'grid'
     statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/cynthiatrainer.jpg')
+    trainerName.setAttribute("src", 'img/claire.png')
     pokesprite1.setAttribute("src", 'img/mawile.png')
     pokesprite2.setAttribute("src", 'img/sylveon.png')
     pokesprite3.setAttribute("src", 'img/tapulele.png')
@@ -39,6 +41,7 @@ function trainer2() {
     pokeSprites.style.display = 'grid'
     statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/redtrainer.jpg')
+    trainerName.setAttribute("src", 'img/jomir.png')
     pokesprite1.setAttribute("src", 'img/incineroar.png')
     pokesprite2.setAttribute("src", 'img/blaziken.png')
     pokesprite3.setAttribute("src", 'img/charizard.png')
@@ -51,6 +54,7 @@ function trainer3() {
     pokeSprites.style.display = 'grid'
     statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/goldtrainer.jpg')
+    trainerName.setAttribute("src", 'img/brandon.png')
     pokesprite1.setAttribute("src", 'img/flygon.png')
     pokesprite2.setAttribute("src", 'img/dragonite.png')
     pokesprite3.setAttribute("src", 'img/hydreigon.png')  
@@ -63,6 +67,7 @@ function trainer4() {
     pokeSprites.style.display = 'grid'
     statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/jamestrainer.jpg')
+    trainerName.setAttribute("src", 'img/haroun.png')
     pokesprite1.setAttribute("src", 'img/gengar.png')
     pokesprite2.setAttribute("src", 'img/golurk.png')
     pokesprite3.setAttribute("src", 'img/doublade.png')   
