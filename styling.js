e4b21e2fe65be18c8bc32208667a850e5c028e95
Claire <claire.isabella.ramos@gmail.com>
2019-11-27 00:53:23 -0500

    added logo for site, filler text for unfortunately not working pokemon information and other odds and ends

--- styling.js
+++ styling.js
@@ -14,34 +14,22 @@ let pokesprite3 = document.getElementById('pokesprite3')
 
 charPortrait.style.display = 'none'
 pokeSprites.style.display = 'none'
-statbox1.style.display = 'none'
-statbox2.style.display = 'none'
-statbox3.style.display = 'none'
+statbox.style.display = 'none'
+
+function statboxOpen() {
+    statbox.style.display = 'grid'
+}
 
 // Function to reveal trainer 1 information
 
 function trainer1() {
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+    statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/cynthiatrainer.jpg')
     pokesprite1.setAttribute("src", 'img/mawile.png')
     pokesprite2.setAttribute("src", 'img/sylveon.png')
     pokesprite3.setAttribute("src", 'img/tapulele.png')
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
 }
 
 // Function to reveal Trainer 2 information
@@ -49,25 +37,11 @@ function pokestat3() {
 function trainer2() {
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+    statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/redtrainer.jpg')
     pokesprite1.setAttribute("src", 'img/incineroar.png')
     pokesprite2.setAttribute("src", 'img/blaziken.png')
     pokesprite3.setAttribute("src", 'img/charizard.png')
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
 }
 
 // Function to reveal Trainer 3 information
@@ -75,25 +49,11 @@ function pokestat3() {
 function trainer3() {
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+    statbox.style.display = 'none'
     gymLeader.setAttribute("src", 'img/goldtrainer.jpg')
     pokesprite1.setAttribute("src", 'img/flygon.png')
     pokesprite2.setAttribute("src", 'img/dragonite.png')
     pokesprite3.setAttribute("src", 'img/hydreigon.png')  
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
 }
 
 // Function to reveal Trainer 4 information
@@ -101,22 +61,9 @@ function pokestat3() {
 function trainer4() {
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+    statbox.style.display = 'none'
+    gymLeader.setAttribute("src", 'img/jamestrainer.jpg')
     pokesprite1.setAttribute("src", 'img/gengar.png')
     pokesprite2.setAttribute("src", 'img/golurk.png')
     pokesprite3.setAttribute("src", 'img/doublade.png')   
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
 }
