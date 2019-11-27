4ea86e0b8f67205e0bfb3a4111e5caf4841d6295
juddin1995 <39656141+juddin1995@users.noreply.github.com>
2019-11-26 21:20:36 -0500

    Async Await

--- library.js
+++ library.js
@@ -1,10 +1,13 @@
 class Trainer {
-    constructor() {
+    constructor(name) {
         this.party = []
+        this.name = name
     }
+
     all = () => {
         return this.party
     }
+
     get = (name) => {
         for (let i = 0; i < this.party.length; i++) {
             if (name == this.party[i].name) {
@@ -12,6 +15,7 @@ class Trainer {
             }
         }
     }
+
     addPokemonToParty(pokemon) {
         if (this.party.length < 12) {
             this.party.push(pokemon)
@@ -21,208 +25,66 @@ class Trainer {
 
 class Pokemon {
     constructor(data) {
-        this.name = data.name;
-        this.sprite = data.sprites.front_default;
-        this.hp = data.stats[4].base_stat;
-        this.attack = data.stats[5].base_stat;
-        this.defense = data.stats[3].base_stat;
-        this.abilities = data.abilities.map(ability => ability.ability.name)  
-    }
-    display() {
-        let grid = document.getElementById("pokedex")
-        let div = document.createElement('div')
-        grid.appendChild(div)
-        div.className = "card"
-        // let pokeImage = document.createElement('img');
-        // pokeImage.src = `${this.sprite}`;
-        // div.appendChild(pokeImage)
-        let name = document.createElement('h2')
-        name.innerHTML = "Name: " + this.name
-        div.appendChild(name)
-        let hp = document.createElement('h3')
-        hp.innerHTML = "HP: " + this.hp
-        div.appendChild(hp)
-        let attack = document.createElement('h3')
-        attack.innerHTML = "Attack: " + this.attack
-        div.appendChild(attack)
-        let defense = document.createElement('h3')
-        defense.innerHTML = "Defense: " + this.defense
-        div.appendChild(defense);
-        let ability = document.createElement('h3')
-        ability.innerHTML = "Ability: " + this.abilities
-        div.appendChild(ability)
-        console.log(this.ability);
+        this.name = data.name
+        this.id = data.id
+        this.stats = {
+            hp: data.stats[5].base_stat,
+            attack: data.stats[4].base_stat,
+            defense: data.stats[3].base_stat
+        }
+        this.abilities = []
+        this.types = []
+
+        for (let i = 0; i < data.abilities.length; i++) {
+            this.abilities.push(data.abilities[i].ability.name)
+        }
+        for (let i = 0; i < data.types.length; i++) {
+            this.types.push(data.types[i].type.name)
+        }
     }
 }
 
-catchIncineroar = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/727.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchBlaziken = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/257.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchCharizard = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/6.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchFlygon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/330.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchFlygon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/330.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchHydreigon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/635.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchDragonite = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/149.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchGengar = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/94.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchGolurk = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/623.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchDoublade = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/680.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchSylveon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/700.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchMawile = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/303.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchTapu_lele = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/786.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
+queryPokemonAPI = async (name) => {
+    let req = await fetch(`https://fizal.me/pokeapi/api/v2/name/${name}.json`)
+    let data = await req.json()
+    let pokemon = new Pokemon(data)
+    console.log(data)
+
+    trainer1.addPokemonToParty(pokemon)
+
+    let pokeGrid = document.getElementById('pokedex')
+    let grid = document.createElement('div')
+    grid.id = (`stat-grid`)
+    pokeGrid.appendChild(grid)
+
+    let nameP = document.createElement('p')
+    nameP.innerText = `${trainer1.party[trainer1.party.length-1].name}`
+    nameP.id = `name`
+    grid.appendChild(nameP)
+
+    let idP = document.createElement('p')
+    idP.innerText = `#${trainer1.party[trainer1.party.length-1].id}`
+    idP.id = `idNum`
+    grid.appendChild(idP)
+
+    let hp = document.createElement('p')
+    hp.innerText = `HP: ${trainer1.party[trainer1.party.length-1].stats.hp}`
+    hp.id = `hp`
+    grid.appendChild(hp)
+
+    let atk = document.createElement('p')
+    atk.innerText = `Attack: ${trainer1.party[trainer1.party.length-1].stats.attack}`
+    atk.id = 'attack'
+    grid.appendChild(atk)
+
+    let def = document.createElement('p')
+    def.innerText = `Defense: ${trainer1.party[trainer1.party.length-1].stats.defense}`
+    def.id = 'defense'
+    grid.appendChild(def)
+
 }
 
-catchIncineroar()
-catchBlaziken()
-catchCharizard()
-catchFlygon()
-catchHydreigon()
-catchDragonite()
-catchGengar()
-catchGolurk()
-catchDoublade()
-catchSylveon()
-catchMawile()
-catchTapu_lele()
-
-let trainer1 = new Trainer()
+let trainer1 = new Trainer('Jomir')
+let trainer2 = new Trainer('Claire')
+let trainer3 = new Trainer('Haroun')
+let trainer4 = new Trainer('Brandon')
