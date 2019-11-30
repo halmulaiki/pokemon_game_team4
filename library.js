b8179765a2a32aa5ad3d12149f0ae096f8eea03d
halmulaiki <56926472+halmulaiki@users.noreply.github.com>
2019-11-30 14:11:21 -0500

    needs more work on the api

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
 class Trainer {
-    constructor(Name) {
-      this.name = name
+    constructor(name) {
         this.party = []
+        this.name = name
     }
     all = () => {
         return this.party
@@ -21,197 +21,61 @@ class Trainer {
 }
 class Pokemon {
     constructor(data) {
-        this.name = data.name;
-        this.sprite = data.sprites.front_default;
-        this.hp = data.stats[4].base_stat;
-        this.attack = data.stats[5].base_stat;
-        this.defense = data.stats[3].base_stat;
-        this.abilities = data.abilities.map(ability => ability.ability.name)
-    }
-    display() {
-        let grid = document.getElementById("pokedex")
-        let div = document.createElement('div')
-        grid.appendChild(div)
-        div.className = "card"
-        // let pokeImage = document.createElement('img');
-        // pokeImage.src = `${this.sprite}`;
-        // div.appendChild(pokeImage)
-        let name = document.createElement('h2')
-        name.innerHTML = "Name: " + this.name
-        div.appendChild(name)
-        let hp = document.createElement('h3')
-        hp.innerHTML = "HP: " + this.hp
-        div.appendChild(hp)
-        let attack = document.createElement('h3')
-        attack.innerHTML = "Attack: " + this.attack
-        div.appendChild(attack)
-        let defense = document.createElement('h3')
-        defense.innerHTML = "Defense: " + this.defense
-        div.appendChild(defense);
-        let ability = document.createElement('h3')
-        ability.innerHTML = "Ability: " + this.abilities
-        div.appendChild(ability)
-        console.log(this.ability);
+        this.name = data.name
+        this.id = data.id
+        this.stats = {
+            hp: data.stats[5].base_stat,
+            attack: data.stats[4].base_stat,
+            defense: data.stats[3].base_stat
+        }
+        this.abilities = []
+        this.types = []
+        for (let i = 0; i < data.abilities.length; i++) {
+            this.abilities.push(data.abilities[i].ability.name)
+        }
+        for (let i = 0; i < data.types.length; i++) {
+            this.types.push(data.types[i].type.name)
+        }
     }
 }
-catchIncineroar = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/727.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer2.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchBlaziken = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/257.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer2.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchCharizard= () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/6.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer2.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
+//
+// function queryPokemonAPI (name)  {
+//  fetch(`https://fizal.me/pokeapi/api/v2/name/${name}.json`).then((resp) =>{
+//  return resp.json()})
+//  .then((data) => {
+//     let pokemon = new Pokemon(data)
+//     console.log(data)
+//     trainer1.addPokemonToParty(pokemon)
+//     let pokeGrid = document.getElementById('pokedex')
+//     let grid = document.createElement('div')
+//     grid.id = (`stat-grid`)
+//     pokeGrid.appendChild(grid)
+//     let nameP = document.createElement('p')
+//     nameP.innerText = `${trainer1.party[trainer1.party.length-1].name}`
+//     nameP.id = `name`
+//     grid.appendChild(nameP)
+//     let idP = document.createElement('p')
+//     idP.innerText = `#${trainer1.party[trainer1.party.length-1].id}`
+//     idP.id = `idNum`
+//     grid.appendChild(idP)
+//     let hp = document.createElement('p')
+//     hp.innerText = `HP: ${trainer1.party[trainer1.party.length-1].stats.hp}`
+//     hp.id = `hp`
+//     grid.appendChild(hp)
+//     let atk = document.createElement('p')
+//     atk.innerText = `Attack: ${trainer1.party[trainer1.party.length-1].stats.attack}`
+//     atk.id = 'attack'
+//     grid.appendChild(atk)
+//     let def = document.createElement('p')
+//     def.innerText = `Defense: ${trainer1.party[trainer1.party.length-1].stats.defense}`
+//     def.id = 'defense'
+//     grid.appendChild(def)
+//
+// })
+// }
 
-catchFlygon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/330.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer3.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchHydreigon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/635.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer3.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchDragonite = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/149.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer3.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchGengar = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/94.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer4.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchGolurk = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/623.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer4.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchDoublade = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/680.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer4.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchSylveon = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/700.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchMawile = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/303.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchTapu_lele = () => {
-    fetch("https://fizal.me/pokeapi/api/v2/id/786.json")
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data => {
-            console.log(data)
-            pokemon = new Pokemon(data)
-            trainer1.addPokemonToParty(pokemon)
-            pokemon.display()
-        }))
-}
-catchIncineroar()
-catchBlaziken()
-catchCharizard()
-catchFlygon()
-catchHydreigon()
-catchDragonite()
-catchGengar()
-catchGolurk()
-catchDoublade()
-catchSylveon()
-catchMawile()
-catchTapu_lele()
-let trainer1 = new Trainer("clair")
-let trainer2 = new Trainer("jomir")
-let trainer3 = new Trainer("brandon")
-let trainer4 = new Trainer("haroun")
+// queryPokemonAPI("mewto")
+let trainer1 = new Trainer('Jomir')
+let trainer2 = new Trainer('Claire')
+let trainer3 = new Trainer('Haroun')
+let trainer4 = new Trainer('Brandon')
--- styling.js
+++ styling.js
@@ -1,6 +1,8 @@
 // Defining the divs from HTML to Javascript for manipulation
 
 let charPortrait = document.getElementById('charPortrait')
+let gymLeader = document.getElementById('gymLeader')
+let trainerName = document.getElementById('trainerName')
 let pokeSprites = document.getElementById('pokeSprites')
 let statbox1 = document.getElementById('statbox1')
 let statbox2 = document.getElementById('statbox2')
@@ -13,106 +15,206 @@ let pokesprite3 = document.getElementById('pokesprite3')
 
 charPortrait.style.display = 'none'
 pokeSprites.style.display = 'none'
-statbox1.style.display = 'none'
-statbox2.style.display = 'none'
-statbox3.style.display = 'none'
+claireStatbox.style.display = 'none'
+jomirStatbox.style.display = 'none'
+brandonStatbox.style.display = 'none'
+harounStatbox.style.display = 'none'
+
+function clairestatboxOpen() {
+    claireStatbox.style.display = 'grid'
+}
+function jomirstatboxOpen() {
+    jomirStatbox.style.display = 'grid'
+}
+
+function brandonstatboxOpen() {
+    brandonStatbox.style.display = 'grid'
+}
+
+function harounstatboxOpen() {
+    harounStatbox.style.display = 'grid'
+}
+
+function trainerpokeAnimation() {
+    charPortrait.classList.add('fadeIn')
+    pokeSprites.classList.add('fadeIn')
+    charName.classList.add('fadeIn')
+    var boxesOpen = 'true'
+    boxesOpen = true
+}
+
+function checkBox() {
+    if (boxesOpen = true) {
+        charPortrait.classList.remove('fadeIn')
+        pokeSprites.classList.remove('fadeIn')
+        charName.classList.remove('fadeIn')
+        charPortrait.classList.add('fadeOut')
+        pokeSprites.classList.add('fadeOut')
+        charName.classList.add('fadeOut')
+        setTimeout(() => {
+            charPortrait.style.display = 'none'
+            pokeSprites.style.display = 'none'
+            charName.style.display = 'none'
+        }, 1500);
+    } else {
+}
+}
 
 // Function to reveal trainer 1 information
 
-function trainer1() {
+function trainers1() {
+
+    // Adding animations to portraits, sprites and changing css displays
+    trainerpokeAnimation()
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+
+    // Closing all statboxes to clean screen
+    claireStatbox.style.display = 'none'
+    jomirStatbox.style.display = 'none'
+    brandonStatbox.style.display = 'none'
+    harounStatbox.style.display = 'none'
+
+    // Displaying trainer image and pokemon sprites
+    gymLeader.setAttribute("src", 'img/cynthiatrainer.jpg')
+    trainerName.setAttribute("src", 'img/claire.png')
     pokesprite1.setAttribute("src", 'img/mawile.png')
     pokesprite2.setAttribute("src", 'img/sylveon.png')
     pokesprite3.setAttribute("src", 'img/tapulele.png')
+    pokesprite1.setAttribute("onclick", 'clairestatboxOpen()')
+    pokesprite2.setAttribute("onclick", 'clairestatboxOpen()')
+    pokesprite3.setAttribute("onclick", 'clairestatboxOpen()')
 }
 
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
-}
 
 // Function to reveal Trainer 2 information
 
-function trainer2() {
+function trainers2() {
+
+    // Adding animations to portraits, sprites and changing css displays
+    charPortrait.classList.add('fadeIn')
+    pokeSprites.classList.add('fadeIn')
+    charName.classList.add('fadeIn')
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+
+    // Closing all statboxes to clean screen
+    claireStatbox.style.display = 'none'
+    jomirStatbox.style.display = 'none'
+    brandonStatbox.style.display = 'none'
+    harounStatbox.style.display = 'none'
+
+    // Displaying trainer image and pokemon sprites
+    gymLeader.setAttribute("src", 'img/redtrainer.jpg')
+    trainerName.setAttribute("src", 'img/jomir.png')
     pokesprite1.setAttribute("src", 'img/incineroar.png')
     pokesprite2.setAttribute("src", 'img/blaziken.png')
     pokesprite3.setAttribute("src", 'img/charizard.png')
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
+    pokesprite1.setAttribute("onclick", 'jomirstatboxOpen()')
+    pokesprite2.setAttribute("onclick", 'jomirstatboxOpen()')
+    pokesprite3.setAttribute("onclick", 'jomirstatboxOpen()')
 }
 
 // Function to reveal Trainer 3 information
 
-function trainer3() {
+function trainers3() {
+
+    // Adding animations to portraits, sprites and changing css displays
+    charPortrait.classList.add('fadeIn')
+    pokeSprites.classList.add('fadeIn')
+    charName.classList.add('fadeIn')
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+
+    // Closing all statboxes to clean screen
+    claireStatbox.style.display = 'none'
+    jomirStatbox.style.display = 'none'
+    brandonStatbox.style.display = 'none'
+    harounStatbox.style.display = 'none'
+
+    // Displaying trainer image and pokemon sprites
+    gymLeader.setAttribute("src", 'img/goldtrainer.jpg')
+    trainerName.setAttribute("src", 'img/brandon.png')
     pokesprite1.setAttribute("src", 'img/flygon.png')
     pokesprite2.setAttribute("src", 'img/dragonite.png')
     pokesprite3.setAttribute("src", 'img/hydreigon.png')
-}
-
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
+    pokesprite1.setAttribute("onclick", 'brandonstatboxOpen()')
+    pokesprite2.setAttribute("onclick", 'brandonstatboxOpen()')
+    pokesprite3.setAttribute("onclick", 'brandonstatboxOpen()')
 }
 
 // Function to reveal Trainer 4 information
 
-function trainer4() {
+function trainers4() {
+
+    // Adding animations to portraits, sprites and changing css displays
+    charPortrait.classList.add('fadeIn')
+    pokeSprites.classList.add('fadeIn')
+    charName.classList.add('fadeIn')
     charPortrait.style.display = 'grid'
     pokeSprites.style.display = 'grid'
-    statbox1.style.display = 'none'
-    statbox2.style.display = 'none'
-    statbox3.style.display = 'none'
+
+    // Closing all statboxes to clean screen
+    claireStatbox.style.display = 'none'
+    jomirStatbox.style.display = 'none'
+    brandonStatbox.style.display = 'none'
+    harounStatbox.style.display = 'none'
+
+    // Displaying trainer image and pokemon sprites
+    gymLeader.setAttribute("src", 'img/jamestrainer.jpg')
+    trainerName.setAttribute("src", 'img/haroun.png')
     pokesprite1.setAttribute("src", 'img/gengar.png')
     pokesprite2.setAttribute("src", 'img/golurk.png')
     pokesprite3.setAttribute("src", 'img/doublade.png')
+    pokesprite1.setAttribute("onclick", 'harounstatboxOpen()')
+    pokesprite2.setAttribute("onclick", 'harounstatboxOpen()')
+    pokesprite3.setAttribute("onclick", 'harounstatboxOpen()')
 }
+// myfunction(303)// statbox1
+// myfunction(700)// statbox2
+// myfunction(786)// statbox3
+// myfunction(727)// statbox4
+// myfunction(257)// statbox5
+// myfunction(6)// statbox6
+// myfunction(330)// statbox7
+// myfunction(149)// statbox8
+// myfunction(635)//statbox9
+// myfunction(94)// statbox10
+// myfunction(623)// statbox11
+// myfunction(680)// statbox12
+// myfunction(25)
+function myfunction  (id) {
+  // console.log("asdfsdfsadfasdfasdfsdafsdfsdafsd");
+ fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`).then((resp) =>{
+ return resp.json()})
+ .then((data) => {
+    let pokemon = new Pokemon(data)
+    console.log(data)
+    trainer1.addPokemonToParty(pokemon)
+    let pokeGrid = document.getElementById('pokedex')
+    let grid = document.createElement('div')
+    grid.id = (`stat-grid`)
+    pokeGrid.appendChild(grid)
+    let nameP = document.createElement('p')
+    nameP.innerText = `${trainer1.party[trainer1.party.length-1].name}`
+    nameP.id = `name`
+    grid.appendChild(nameP)
+    let idP = document.createElement('p')
+    idP.innerText = `#${trainer1.party[trainer1.party.length-1].id}`
+    idP.id = `idNum`
+    grid.appendChild(idP)
+    let hp = document.createElement('p')
+    hp.innerText = `HP: ${trainer1.party[trainer1.party.length-1].stats.hp}`
+    hp.id = `hp`
+    grid.appendChild(hp)
+    let atk = document.createElement('p')
+    atk.innerText = `Attack: ${trainer1.party[trainer1.party.length-1].stats.attack}`
+    atk.id = 'attack'
+    grid.appendChild(atk)
+    let def = document.createElement('p')
+    def.innerText = `Defense: ${trainer1.party[trainer1.party.length-1].stats.defense}`
+    def.id = 'defense'
+    grid.appendChild(def)
 
-function pokestat1() {
-    statbox1.style.display = 'grid'
-}
-
-function pokestat2() {
-    statbox2.style.display = 'grid'
-}
-
-function pokestat3() {
-    statbox3.style.display = 'grid'
+})
 }
